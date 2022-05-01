57c77428a762efb03e7f38bbaaf8b8a352d111b4
jhon3242 <rldotnr2@gmail.com>
2022-05-01 13:52:27 +0900

    할인 정책 변경을 DI 로 구현 및 테스트 코드

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,12 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-	private final MemberReposiroty memberReposiroty = new MemoryMemberRepository();
+//	private final MemberReposiroty memberReposiroty = new MemoryMemberRepository(); 이것은 마치 배우가 직접 담당 배역을 설정
+	private final MemberReposiroty memberReposiroty;
+
+	public MemberServiceImpl(MemberReposiroty memberReposiroty) {
+		this.memberReposiroty = memberReposiroty;
+	}
 
 	@Override
 	public void join(Member member) {
---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,30 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FIxDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberReposiroty;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+//	private final MemberReposiroty memberReposiroty = new MemoryMemberRepository();
+//	private final DiscountPolicy discountPolicy = new FIxDiscountPolicy();
+//	private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+	private final MemberReposiroty memberReposiroty;
+	private final DiscountPolicy discountPolicy;
+
+	public OrderServiceImpl(MemberReposiroty memberReposiroty, DiscountPolicy discountPolicy) {
+		this.memberReposiroty = memberReposiroty;
+		this.discountPolicy = discountPolicy;
+	}
+
+	@Override
+	public Order createOrder(Long memberId, String itemName, int itemPrice) {
+		Member member = memberReposiroty.findbyID(memberId);
+		int discountPrice = discountPolicy.discount(member, itemPrice);
+
+		return new Order(memberId, itemName, itemPrice, discountPrice );
+	}
+}
