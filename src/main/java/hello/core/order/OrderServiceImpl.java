c36fb58f8c8ab641149c51f9267eb68d49dd52e4
jhon3242 <rldotnr2@gmail.com>
2022-05-11 15:58:33 +0900

    스프링 빈 중복시 해결 방법 공부

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,15 +9,21 @@ import hello.core.member.MemberReposiroty;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
 	private MemberReposiroty memberReposiroty;
 	private DiscountPolicy discountPolicy;
 
+	@Autowired
+	public OrderServiceImpl(MemberReposiroty memberReposiroty, DiscountPolicy discountPolicy) {
+		this.memberReposiroty = memberReposiroty;
+		this.discountPolicy = discountPolicy;
+	}
 
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
