f4f1adee5bfa67049357522ff4f028468e3d044e
jhon3242 <rldotnr2@gmail.com>
2023-04-23 15:24:26 +0900

    feat (All) : DI 의존관계 주입

--- src/test/java/hello/core/member/MemberServiceImplTest.java
+++ src/test/java/hello/core/member/MemberServiceImplTest.java
@@ -1,19 +1,21 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceImplTest {
-	MemberService memberService = new MemberServiceImpl();
+	AppConfig appConfig = new AppConfig();
+	MemberService memberService = appConfig.memberService();
 
 	@Test
 	void join() {
 		// given
 		Member member = new Member(1L, "memberA", Grade.BASIC);
 
-		// when현
+		// when
 		memberService.join(member);
 		Member findMember = memberService.findMember(1L);
 
